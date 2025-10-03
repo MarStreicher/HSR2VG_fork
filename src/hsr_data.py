d36246511ab1ee2acc57a009784ef678d5fcea00
MarStreicher <mstreicher@uni-potsdam.de>
2025-10-03 13:12:16 +0200

    Added LMA jobs

--- src/hsr_data.py
+++ src/hsr_data.py
@@ -3,9 +3,9 @@ from sklearn.model_selection import train_test_split
 
 
 class HsrData:
-    def __init__(self, domain: str):
+    def __init__(self, domain: str, label: str):
         self.DOMAIN = domain.capitalize()
-        self.LABEL = "N"
+        self.LABEL = label
         self.DATA_PATH = f"data/UNL_{self.DOMAIN}_measured_reflectance_param.csv"
         self._load_data()
 
@@ -13,5 +13,9 @@ class HsrData:
         frame = pd.read_csv(self.DATA_PATH)
         self.hsr_columns = [str(wavelength) for wavelength in range(400, 1401)]
         self.feature_matrix = frame[self.hsr_columns]
-        self.labels = frame[self.LABEL]
+        if self.LABEL in frame.columns:
+            self.labels = frame[self.LABEL]
+        else:
+            raise NotImplementedError(f"{self.LABEL} not in the data frame.")
+
         return self
